dc9900475ab01c80d2d6eb2e1de686f9ea4ee8eb
Lilliam Arce <lilliam.arce@creativedrive.com>
2020-07-04 15:32:49 -0600

    Added header syle

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :products
-  get 'pages/index'
   root 'pages#index'
+
+  PagesController.action_methods.each do |action|
+    get "/#{action}", to: "pages##{action}", as: "#{action}_page"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
