30f34162e293973434910e7cf6d18fa2fce68964
Lilliam Arce <lilliam.arce@creativedrive.com>
2020-07-24 14:43:28 -0600

    Added logic for upload images with carrierwave, bugs with repeted configuration

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
     belongs_to :category
     has_many :uploads, as: :media
+    accepts_nested_attributes_for :uploads, allow_destroy: true
 end
---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = "dvbyibq3r"
+    config.api_key = "868184375377887"
+    config.api_secret = "PV-ec5GnaNHKLpoHuZmkgwViAG8"
+    config.secure = true
+    config.cdn_subdomain = true
+end
